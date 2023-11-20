2733aca988fc65ece623aff54e09387a2fb5da95
Ajay Bhuj <bhujajay9@gmail.com>
2023-11-20 22:34:42 +0530

    first project completed

---
+++ Color Scheme Switcher/index.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body");
+const btns = body.querySelectorAll(".button");
+
+btns.forEach((b) => {
+  b.addEventListener("click", (e) => {
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
